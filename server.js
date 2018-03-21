3b7bf64a2ee80968f37d5dceecaa6e51d4916b80
Priyanka Phapale <pbphapale@gmail.com>
2018-03-20 23:00:52 -0400

    Updating angular version and folder structure

--- server.js
+++ server.js
@@ -1,32 +1,51 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-var morgan = require('morgan');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var router = express.Router();
-var appRoutes = require('./app/routes/api')(router);
-var path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const path = require('path');
+const cors = require('cors');
+const passport = require('passport');
+const config = require('./config/database');
 
+//Connect to Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected',() => {
+    console.log('Connected to database' + config.database);
+});
+
+// On Error
+mongoose.connection.on('error',(err) => {
+    console.log('Not Connected to database' + err);
+});
+
+const app = express();
+
+const users = require("./routes/users");
+
+//PORT Number
+const port = process.env.PORT || 3000;
+
+// CORS Middleware
+app.use(cors());
+
+// Set static Folder
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(morgan('dev'));
+// Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
-app.use('/api',appRoutes);
-
-mongoose.connect('mongodb://localhost:27017/test_mean',function(err){
-    if(err){
-        console.log('Not connected to MongoDB '+err);
-    }else{
-        console.log('Successfully Connected..');
-    }
-});
+app.use('/users',users);
 
 app.get('*',function(req,res){
     res.sendFile(path.join(__dirname+'/public/app/views/index.html'));
 });
 
+//Start Server
 app.listen(port, function(){
     console.log("Running the server on port " + port);
 });
